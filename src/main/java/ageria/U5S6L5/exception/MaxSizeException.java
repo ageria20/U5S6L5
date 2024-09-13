a3acdb2577b64e8eba279e4506b83800200f3248
Andrea <ageria20@gmail.com>
2024-09-13 15:54:54 +0200

    chenges

--- src/main/java/ageria/U5S6L5/exception/MaxSizeException.java
+++ src/main/java/ageria/U5S6L5/exception/MaxSizeException.java
@@ -1,9 +1,10 @@
 package ageria.U5S6L5.exception;
 
+import com.cloudinary.api.exceptions.BadRequest;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 public class MaxSizeException extends MaxUploadSizeExceededException {
-    public MaxSizeException(Long id) {
-        super(id);
+    public MaxSizeException(Long fileSize) {
+        super(fileSize);
     }
 }
